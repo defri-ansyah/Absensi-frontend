c7c0b836f55efb757ccf95c984c2ce42335acfd8
defri-ansyah <defriansyah013@gmail.com>
2021-09-18 00:49:52 +0700

    edit router-link

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,16 +1,16 @@
 <template>
   <aside class="d-flex flex-column">
     <div>
-      <router-link to="/" class="d-flex m-5">
+      <router-link to="/home" class="d-flex m-5">
         <img src="../assets/grid.svg" alt="grid" />
         <p>Dashboard</p>
       </router-link>
-      <router-link to="/manage-phone" class="d-flex m-5">
+      <router-link to="/profile" class="d-flex m-5">
         <img src="../assets/user.svg" alt="profile" />
         <p>Profile</p>
       </router-link>
     </div>
-    <router-link to="/login" class="d-flex m-5">
+    <router-link to="/" class="d-flex m-5">
       <img src="../assets/log-out.svg" alt="logout" />
       <p>Logout</p>
     </router-link>
